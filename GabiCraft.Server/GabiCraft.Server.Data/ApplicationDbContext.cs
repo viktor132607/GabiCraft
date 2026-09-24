52c3c4e17563d171ec7529feed56d79b95f7a822
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-09-25 02:24:27 +0300

    refactor: reduce project to static frontend

--- GabiCraft.Server/GabiCraft.Server.Data/ApplicationDbContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace GabiCraft.Server.Data;
-
-public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-    : DbContext(options)
-{
-}
